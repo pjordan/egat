1f9d78ff1a1d5d4b414fdf1576c899cbe03b4586
Patrick Jordan <pjordan@zebra.reasoningtech.com>
2009-03-03 20:52:33 -0500

    update finder

--- min-curb/src/main/java/edu/umich/eecs/ai/egat/mincurb/BenischMinimumCurbFinder.java
+++ min-curb/src/main/java/edu/umich/eecs/ai/egat/mincurb/BenischMinimumCurbFinder.java
@@ -2,6 +2,7 @@ package edu.umich.eecs.ai.egat.mincurb;
 
 import edu.umich.eecs.ai.egat.game.SymmetricGame;
 import edu.umich.eecs.ai.egat.game.Action;
+import edu.umich.eecs.ai.egat.game.Strategy;
 
 import java.util.*;
 
@@ -45,10 +46,44 @@ public class BenischMinimumCurbFinder implements MinimumCurbFinder {
         return currentCurb;
     }
 
+    protected SymmetricGame findMinimumCurb(Action seed, SymmetricGame game) {
+        SymmetricGame currentCurb = mccFinder.findMinContainingCurb(seed, game);
+        
+        List<Action> remainingActions = new ArrayList<Action>(game.getActions());
+        remainingActions.remove(seed);
+
+        boolean flag = true;
+
+        while (flag) {
+            flag = false;
+
+            for (Action a : currentCurb.getActions()) {
+                if (remainingActions.contains(a)) {
+                    remainingActions.remove(a);
+
+                    SymmetricGame candidate = mccFinder.findMinContainingCurb(a, currentCurb);
+
+                    if (candidate.getActions().size() < currentCurb.getActions().size()) {
+                        currentCurb = candidate;
+
+                        flag = true;
+
+                        break;
+                    }
+                }
+            }
+        }
+
+        return currentCurb;
+    }
+
     public Set<SymmetricGame> findAllMinimumCurbs(SymmetricGame game) {
         Map<Action, SymmetricGame> curbs = new HashMap<Action, SymmetricGame>();
+        Map<Set<Action>, SymmetricGame> games = new HashMap<Set<Action>, SymmetricGame>();
 
         Set<SymmetricGame> minCurbs = new HashSet<SymmetricGame>();
+
+        minCurbs.add(game);
 
         for (Action action : game.getActions()) {
 
@@ -59,18 +94,30 @@ public class BenischMinimumCurbFinder implements MinimumCurbFinder {
             }
 
             if (curbGame.getActions().contains(action)) {
-                SymmetricGame reducedGame = mccFinder.findMinContainingCurb(action, curbGame);
+                SymmetricGame reducedGame = findMinimumCurb(action, curbGame);
 
-                if (reducedGame.getActions().size() < curbGame.getActions().size()) {
-
-                    for (Action a : curbGame.getActions()) {
-                        curbs.put(a, reducedGame);
-                    }
-
-                    minCurbs.remove(curbGame);
-                    minCurbs.add(reducedGame);
+                if(!games.containsKey(reducedGame.getActions())) {
+                    games.put(reducedGame.getActions(), reducedGame);        
                 }
 
+                reducedGame = games.get(reducedGame.getActions());
+
+
+                if (reducedGame.getActions().size() < curbGame.getActions().size()) {
+                    if (minCurbs.contains(curbGame)) {
+                        minCurbs.remove(curbGame);
+                    }
+
+                    if (!minCurbs.contains(reducedGame)) {
+                        minCurbs.add(reducedGame);
+                    }
+
+                    for (Action a : reducedGame.getActions()) {
+                        if (curbs.get(a) == null || reducedGame.getActions().size() < curbs.get(a).getActions().size()) {
+                            curbs.put(a, reducedGame);
+                        }
+                    }
+                }
             }
         }
 
